97d8fc448b04da2472ca987f04a247245725eef2
crsnpalmer-art <crsnpalmer@gmail.com>
2026-03-01 20:24:45 -0600

    cleanup: remove alternatives/tools pages, redirect roadmap+how-we-research to home

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: "/roadmap", destination: "/", permanent: false },
+      { source: "/how-we-research", destination: "/blog", permanent: false },
+      { source: "/alternatives", destination: "/", permanent: false },
+      { source: "/tools", destination: "/", permanent: false },
+      { source: "/tools/:path*", destination: "/", permanent: false },
+    ];
+  },
+};
 module.exports = nextConfig;
